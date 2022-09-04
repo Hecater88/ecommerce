7fcbe62f2ad742a9d1a7c7e3aa19017e7bc40ae9
Virgilio <47868066+Hecater88@users.noreply.github.com>
2022-09-04 16:38:56 +0200

    Hook up Sanity

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
-const Home = () => {
+import { client } from "../lib/client";
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      {console.log("bannerData", bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Prduct 1", "Prduct 2", "Prduct 3"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
